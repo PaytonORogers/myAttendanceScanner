a2b85f39a4b92a8cbd762610fbcc2cf88a703351
Josh Noll <noll.joshua@outlook.com>
2025-08-22 16:04:40 -0400

    pushin to main on a Friday YOLO

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,32 +1,14 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [names, setNames] = useState(["loading"]);
-
-  useEffect(() => {
-    if (names[0] !== "loading") return;
-    const getNames = async () => {
-      try {
-        const data = await fetch("http://localhost:8080/", {
-          
-        });
-        if (!data.ok) throw new Error("Weird");
-        const dataJson = await data.json();
-        setNames(dataJson.map((item) => item.colName));
-      } catch (err) {
-        console.log(err.message);
-      }
-    };
-    getNames();
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="bg-[red]">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -34,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{names.join(", ")} Workshop</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -47,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
